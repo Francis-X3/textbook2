578dcd622c5e052801875521ab8c69f87e688455
Francis Xavier <francisfranky24@gmail.com>
2022-02-17 15:29:35 +0530

    cpndition

--- portal/src/App.js
+++ portal/src/App.js
@@ -2,14 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/form'
 import Login from './components/login'
+import Register from './components/register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
 
 function App() {
+  const [user, setLoginUser] = useState({})
   return (
-    <div className="App">
-      {/* <Login /> */}
+    <>
+      <div className="App">
+        {/* <Login />
       <Form />
-      {/* <Register /> */}
-    </div>
+      <Register /> */}
+        <Router>
+          <Routes>
+            <Route exact path='/' />
+            <Route exact path='/form' element={<Form />} />
+            /
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/register' element={<Register />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
