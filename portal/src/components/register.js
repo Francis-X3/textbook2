d2ed0982bd77e51c001d4e9393c6e4f739f3720c
Francis Xavier <francisfranky24@gmail.com>
2022-02-17 16:19:57 +0530

    css

--- portal/src/components/register.js
+++ portal/src/components/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import './register.css'
 import { useNavigate } from 'react-router-dom'
 
 export default function Register() {
@@ -30,19 +31,21 @@ export default function Register() {
 
   const navigate = useNavigate()
   return (
-    <div>
-      <ul>
+    <div className='login'>
+      <ul className='form'>
+        <form className='form-elements' >
         <h1>Register</h1>
         <input name='name' value={user.name} placeholder='Your Name' type="text" onChange={handleChange} />
         <input name='email' value={user.email} placeholder='Your Email' type="email" onChange={handleChange} />
         <input name='password' value={user.password} placeholder='Password' type="password" onChange={handleChange} />
         <input name='reEnterPassword' value={user.reEnterPassword} placeholder='Reenter Password' type="password" onChange={handleChange} />
-      </ul>
       <div>
         <button onClick={register} type='submit'>Register</button>
-        or
+        <li>OR</li>
         <button onClick={() => navigate("/login")} type='submit'>login</button>
       </div>
+        </form>
+      </ul>
     </div>
   )
 }
